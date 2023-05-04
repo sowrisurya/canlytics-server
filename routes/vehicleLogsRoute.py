8a7529b65ab5b867a6a171c18f326380b91746d5
sowri <sowrisurya@outlook.com>
2023-05-04 20:02:38 +0100

    modified route for the logs all

--- routes/vehicleLogsRoute.py
+++ routes/vehicleLogsRoute.py
@@ -14,7 +14,7 @@ vehicleLogsRouter = APIRouter(
 )
 
 # Get all vehicleLogs
-@vehicleLogsRouter.get("/{vehcile_id}", response_model=List[VehicleLogsObject])
+@vehicleLogsRouter.get("/all", response_model=List[VehicleLogsObject])
 async def get_all_vehicleLogs(vehicle_id: str, limit: int = 100, start_time: datetime.datetime = None, end_time: datetime.datetime = None):
 	vehicleLogs = VehicleLogsController.get_vehicle_logs(vehicle_id = vehicle_id, start_time = start_time, end_time = end_time, limit = limit)
 	if vehicleLogs:

ca11bf4021a48eac6c95a27ea6dbfda486d88692
sowri <sowrisurya@outlook.com>
2023-05-08 15:24:49 +0100

    modified mqtt client and added new certificates

--- subscribers/dataAdderQueue.py
+++ subscribers/dataAdderQueue.py
@@ -1,13 +1,7 @@
 from utils import MQTTClient
 import time, json, asyncio
 from utils import MQTT_TOPIC, REDIS_CLIENT
-import logging
 
-fh = logging.FileHandler('dataAdderQueue.log')
-fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-logger = logging.getLogger(__name__)
 
 class DataAdderQueue(object):
 	def __init__(self):
@@ -15,10 +9,10 @@ class DataAdderQueue(object):
 		self.__subscriber = REDIS_CLIENT.pubsub()
 		self.__subscriber.subscribe("dataAdderPublish")
 		self.__crnt_msg = None
-		# self.__callbacks = callbacks
 
 	# def add_data(self, data):
 	# 	REDIS_QUEUE.enqueue(self.__mqtt_client.publish, MQTT_TOPIC, data)
+
 	def wait_for_messages(self):
 		while True:
 			msg = self.__subscriber.get_message(ignore_subscribe_messages=True)
@@ -28,7 +22,7 @@ class DataAdderQueue(object):
 					continue
 				self.__crnt_msg = data
 				self.__mqtt_client.publish(MQTT_TOPIC, data["data"])
-				# await self.wait_for_mqtt_msg(testing = False, timeout = 30)
+				self.wait_for_mqtt_msg(timeout = 30)
 				time.sleep(2)
 			else:
 				time.sleep(0.1)
@@ -41,53 +35,38 @@ class DataAdderQueue(object):
 				if not str(data["data"]).startswith("server>"):
 					continue
 				self.__crnt_msg = data
-				print("Publishing: " + data["data"] + " to " + MQTT_TOPIC + " topic")
 				self.__mqtt_client.publish(MQTT_TOPIC, data["data"])
-				# await self.wait_for_mqtt_msg(testing = False, timeout = 30)
+				await self.wait_for_mqtt_msg(testing = False, timeout = 30)
 				await asyncio.sleep(2)
 			else:
 				await asyncio.sleep(0.1)
-		# for msg in self.__subscriber.listen():
-		# 	if msg is not None and msg["type"] == "message":
-		# while True:
-		# 	msg = self.__subscriber.get_message()
-		# 	if msg is not None:
-		# 		self.__crnt_msg = msg
-		# 		break
-		# 	else:
-		# 		time.sleep(0.1)
 
-	async def wait_for_mqtt_msg(self, timeout = 10, testing = True, testing_message = "client_id:simcom_client01|62 F1 90 53 42 4D 31 36 41 45 42 38 4E 57 30 30 30 32 34 35"):
+	def wait_for_mqtt_msg(self, timeout = 10):
 		start_time = time.time()
 		while self.__crnt_msg is not None and time.time() - start_time < timeout:
-			asyncio.sleep(0.1)
+			time.sleep(0.1)
+
+	async def wait_for_mqtt_msg_async(self, timeout = 10, testing = True, testing_message = "client_id:simcom_client01|62 F1 90 53 42 4D 31 36 41 45 42 38 4E 57 30 30 30 32 34 35"):
+		start_time = time.time()
+		while self.__crnt_msg is not None and time.time() - start_time < timeout:
+			await asyncio.sleep(0.1)
 		if testing:
 			self.recv_data_callback(testing_message)
 
 	def recv_data_callback(self, message):
-	# def recv_data_callback(self, client, userdata, message):
-		data = message
-		# try:
-		# 	data = message.payload.decode("utf-8")
-		# except Exception as e:
-		# 	data = message
-		logger.debug("Received: " + data)
-		print("Received: " + data)
+		try:
+			data = message.payload.decode("utf-8")
+		except Exception as e:
+			data = message
 		if data is None or not isinstance(data, str):
 			return
 		if data.startswith("server>"):
 			return
 		else:
 			data = data.lower()
-			print("Publishing: " + data + " to dataAdderSubscribe topic")
 			crnt_msg = self.__crnt_msg
 			REDIS_CLIENT.publish("dataAdderSubscribe", json.dumps({"crnt_msg": crnt_msg, "data": data}))
 			self.__crnt_msg = None
-			# for callback in self.__callbacks:
-			# 	callback(data)
-
 
 	def configure(self):
 		self.__mqtt_client.subscribe(MQTT_TOPIC)
-
-# DATA_ADDER_QUEUE = DataAdderQueue()
--- utils/mqttClient.py
+++ utils/mqttClient.py
@@ -24,31 +24,20 @@ class MQTTClient(object):
 
 	def __init__(self, client_id = MQTT_CLIENT_ID, host = MQTT_HOST, rootCAPath = MQTT_ROOT_CA, privateKeyPath = MQTT_PRIVATE_KEY, certificatePath = MQTT_CERTIFICATE, port = 8883, callback = None):
 		self.__client = mqtt5_client_builder.mtls_from_path(
-			# client_options = mqtt5.ClientOptions(
-			# 	host_name = host,
-			# 	session_behavior=mqtt5.ClientSessionBehaviorType.REJOIN_ALWAYS,
-			# 	connack_timeout_ms = 5000,
-			# ),
 			endpoint = host,
 			port = port,
 			cert_filepath = certificatePath,
 			pri_key_filepath = privateKeyPath,
 			ca_filepath = rootCAPath,
 			client_id = client_id,
-			# on_connection_interrupted = self.on_connection_interrupted,
-			# on_connection_resumed = self.on_connection_resumed,
-			# clean_session = False,
 			on_publish_received = self.on_publish_callback_fn,
 			on_lifecycle_stopped = self.on_lifecycle_stopped,
 			on_lifecycle_connection_success = self.on_lifecycle_connection_success,
 			on_lifecycle_connection_failure = self.on_lifecycle_connection_failure,
-			# keep_alive_interval_sec = 5,
 		)
 		self.__callback = callback
 		self.__future_stopped = Future()
 		self.__future_connection_success = Future()
-
-		# self.__recevd_all_event = threading.Event()
 
 		self.__client.start()
 		lifecycle_connect_success_data = self.__future_connection_success.result(TIMEOUT)
@@ -56,30 +45,11 @@ class MQTTClient(object):
 		negotiated_settings = lifecycle_connect_success_data.negotiated_settings
 		print(f"Connected to endpoint:'{host}' with Client ID:'{client_id}' with reason_code:{repr(connack_packet.reason_code)}")
 
-		# self.__client.connect()
-
-		# self.__client = AWSIoTMQTTClient(client_id)
-		# self.__client.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
-		# self.__client.configureEndpoint(host, port)
-
-		# self.__client.configureAutoReconnectBackoffTime(1, 32, 10)
-		# self.__client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-		# self.__client.configureDrainingFrequency(2)  # Draining: 2 Hz
-		# self.__client.configureConnectDisconnectTimeout(10)  # 10 sec
-		# self.__client.configureMQTTOperationTimeout(60)  # 5 sec
-
-		# self.__client.connect()
-
 	def on_publish_callback_fn(self, publish_packet):
-		print("received publish packet:")
-		# self.__recevd_all_event.set()
 		publish_packet = publish_packet.publish_packet
 		assert isinstance(publish_packet, mqtt5.PublishPacket)
 		if self.__callback is not None:
-			self.__callback(publish_packet.payload.decode())
-		# print("Publish callback: " + str(publish_packet.topic) + " " + str(publish_packet.payload))
-		# print("Publish callback: " + str(payload))
-		# print("Publish callback: " + str(data))
+			self.__callback(publish_packet)
 
 	def on_connection_interrupted(self, connection, error, **kwargs):
 		print(f"MQTT connection interrupted: {error}")
@@ -96,9 +66,10 @@ class MQTTClient(object):
 	# Callback for the lifecycle event Connection Success
 	def on_lifecycle_connection_success(self, lifecycle_connect_success_data: mqtt5.LifecycleConnectSuccessData):
 		print("Lifecycle Connection Success")
-		# self.__client.start()
-		self.__future_connection_success.set_result(lifecycle_connect_success_data)
-
+		if not self.__future_connection_success.done():
+			self.__future_connection_success.set_result(lifecycle_connect_success_data)
+		else:
+			print("Connection success already received")
 
 	# Callback for the lifecycle event Connection Failure
 	def on_lifecycle_connection_failure(self, lifecycle_connection_failure: mqtt5.LifecycleConnectFailureData):
@@ -112,7 +83,6 @@ class MQTTClient(object):
 	def ackCallback(self, mid, data):
 		print("Received ack for message: " + str(mid) + " data: " + str(data))
 
-	# def subscribe(self, topic, callback, async_subscribe = False):
 	def subscribe(self, topic):
 		print(f"Subscribing to topic: {topic}")
 		subscribr_future = self.__client.subscribe(
@@ -123,16 +93,9 @@ class MQTTClient(object):
 			)
 		)
 		subscribr_future.result()
-		# if async_subscribe:
-		# 	self.__client.subscribeAsync(topic, QoS = 1, ackCallback = self.ackCallback, messageCallback = callback)
-		# else:
-		# 	self.__client.subscribe(topic, QoS = 1, callback = callback)
 
 	def publish(self, topic, message) -> bool:
 		pubs = self.__client.publish(
-			# topic = topic,
-			# payload = message,
-			# qos = mqtt.QoS.EXACTLY_ONCE
 			publish_packet = mqtt5.PublishPacket(
 				topic = topic,
 				payload = message,
@@ -140,15 +103,9 @@ class MQTTClient(object):
 			)
 		)
 		pubs.result()
-		# time.sleep(1)
-		# logger.info(f"Publishing message: {message} to topic: {topic}")
-		# return self.__client.publish(topic, message, 1)
 
 	def unsubscribe(self, topic):
 		self.__client.unsubscribe(topic)
-
-	# def wait(self):
-	# 	self.__recevd_all_event.wait()
 
 	def disconnect(self):
 		self.__client.stop()
